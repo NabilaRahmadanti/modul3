1db7d5d5c2ff3a5a8cdcb0f566044c112658f376
Your Name <you@example.com>
2019-01-08 16:54:17 +0700

    selesai

---
+++ latihan3.php
@@ -0,0 +1,9 @@
+<?php
+    $user = "nabila";
+    $pass = "123";
+    if ($user == "nabila" && $pass == "123") {
+        echo "Login Berhasil";
+    } else {
+        echo "Login Gagal";
+    }   
+?>
